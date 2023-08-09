2d8d06d8472822bfcd682574b7234cf4f6b3c894
SahilRawat <sahilrawat696@gmail.com>
2023-08-09 20:28:21 +0530

    commit

--- Algorithm_Visualizer/my-app/src/components/Home.js
+++ Algorithm_Visualizer/my-app/src/components/Home.js
@@ -1,50 +1,56 @@
 import React, { Component } from "react";
-import "../styles/home.css"
-import bar from "../saves/bar-chart.svg";
-class Home extends Component{
-    constructor(){
-        super();
-        this.state={
-            problems:[
-                {
-                    name:"Sorting",
-                    imgUrl:bar,
-                    link:"/sortingpage"
-                }
-            ]
-        }
-    }
-    // componentDidMount(){
-        
-    //   window.particlesJS("particles-js",particleJSON );
-      
-    // }
-    render(){
-        return(
-            <div className="back">
-                <div id="particles-js"></div>
-              
-                    
-                <h1 className="main-title center">
-                            {/* <div className="logo">
-                            <LogoIcon/>
-                            </div> */}
-                            ALGORITHM <strong style={{color:"var(--home-color)"}}><br></br>VISUALIZER</strong>
-                </h1>
-                <ul class="list-inline mt-5" style={{display:"block",position:"relative"}}>
-                    {this.state.problems.map(element=>
-                        <li className="list-inline-item ml-5 mr-5 mb-2" style={{cursor:"pointer"}} onClick={()=>window.location.href=element.link}>
-                        <div className="card" style={{width: "14rem"}}>
-                            <img className="card-img-top img algo-image" src={element.imgUrl} alt="Card image cap"/>
-                            <div style ={{backgroundColor:"transparent"}} className="card-body">
-                                <h3 className="card-text algo-name">{element.name}</h3>
-                            </div>
-                        </div>
-                        </li>
-                    )}
-                </ul>
+import "../styles/home.css";
+import bar from "../saves/sort1.gif";
+import ds from "../saves/ds1.png";
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      problems: [
+        {
+          name: "Sorting",
+          imgUrl: bar,
+          link: "/sortingpage",
+        },
+        {
+          name: "Data Structures",
+          imgUrl: ds,
+          link: "/datastructure",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="back">
+      <div className="back1">
+        <h1 className="main-title">
+          ALGORITHM
+          <strong style={{ color: "var(--home-color)" }}>
+            <br />
+            VISUALIZER
+          </strong>
+        </h1>
+        </div>
+        <div className="imglink">
+          {this.state.problems.map(element =>
+            <div
+              className="item"
+              style={{ cursor: "pointer" }}
+              onClick={() => (window.location.href = element.link)}
+            >
+              <div className="demo">
+                <img src={element.imgUrl} alt="Card cap" height={175} width={175}/>
+                  <h3 className="algo-name">{element.name}</h3>
+                </div>
             </div>
-        )
-    }
+          )}
+        </div>
+      </div>
+    );
+  }
 }
+
 export default Home;
